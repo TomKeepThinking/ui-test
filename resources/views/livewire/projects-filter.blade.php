b6dcd316729e81bb85d9a0fe7672c7c26df007e6
tom <thomasscullyking@hotmail.com>
2023-12-21 13:56:24 +0000

    Fixed footer border issue

--- resources/views/livewire/projects-filter.blade.php
+++ resources/views/livewire/projects-filter.blade.php
@@ -1,21 +1,21 @@
 <div class="mb-48" x-data="{'activeImage': ''}">
     <div class="flex justify-between items-center mb-8 text-grey">
 
-        <div class="flex space-x-4  capitalize">
+        <div class="flex flex-wrap capitalize">
             @foreach($filters as $filter)
-                <div wire:click="setActiveItem(@js($filter))" @class(['cursor-pointer', 'underline !text-charcoal' => $filter === $activeFilter])>
+                <div wire:click="setActiveItem(@js($filter))"
+                    @class(['cursor-pointer mr-4', 'underline !text-charcoal' => $filter === $activeFilter])>
                     {{$filter}}
                 </div>
             @endforeach
         </div>
     </div>
 
-
     <div class="relative">
         @foreach($filters as $filter)
             <div x-cloak
                  x-show="$wire.activeFilter === @js($filter)"
-                 class="grid grid-cols-12 gap-5 gap-y-24"
+                 class="grid grid-cols-12 gap-x-5 gap-y-36"
                  x-transition:enter.opacity.duration.500ms>
 
                 @foreach($filteredItems as $key=>$item)
